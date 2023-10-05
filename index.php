73c7cbf5edff7c15d043c3b456a1fa7bbeb44063
angelfo7319 <144272766+angelfo7319@users.noreply.github.com>
2023-10-04 22:36:12 -0400

    Connected index.php to the databse, changed inputs

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
-<!DOCTYPE html>
 <html>
     <head>
-        <title>Edu Planner</title>    
-        <link rel="stylesheet" href="css/style.css">
+        <title>Edu Planner</title>
+
+        <!-- Links the CSS file to the PHP file -->
+        <?php echo '<link rel="stylesheet" type="text/css" href="style.css"></head>'; ?>
     </head>
 
     <body>
         <div class="login-box">
             <h2>Login</h2>
-            <form>
+            <form method="post" action="">
               <div class="user-box">
-                <input type="text" name="" required="">
+                <input type="text" name="email">
                 <label>Username</label>
               </div>
               <div class="user-box">
-                <input type="password" name="" required="">
+                <input type="password" name="password">
                 <label>Password</label>
               </div>
-              <a href="#">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                Submit
-              </a>
+              <input type="submit" name="submit">
+            
             </form>
+            <div class = "echo-statement">
+            <?php include("loginArithmetic.php"); ?>
+            </div>
           </div>
     </body>
+    </style>
+
 </html>
