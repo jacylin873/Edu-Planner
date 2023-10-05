d4071373a69e7a7b7132fc21141cfcb68a0eb041
angelfo7319 <144272766+angelfo7319@users.noreply.github.com>
2023-10-04 22:40:35 -0400

    Connect file that works.

--- includes/connect.php
+++ includes/connect.php
@@ -1,33 +1,26 @@
 <?php
-//$path = '/includes';
-//set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
-//Database location on server.
+//Database location on server
 $server = "localhost";
-//Database user name. Use root for now.
+//Database username
 $dbusername = "root";
-//Database password leave blank for now.
+//Database password
 $password = "";
-//Database name.
-$db = "starter";
-//Can use to create your own debug. Leave true to show if you connect then change to false.
+//Database name
+$db = "edu_planner";
+//Check for debug
 $debug = "true";
 
-//Set up the database connector
-$dbconn = mysqli_connect($server, $dbusername, $password, $db);
+$conn = mysqli_connect($server, $dbusername, $password, $db);
 
-//Show errors if connection failed
-if ($dbconn->connect_error) {
-    die('Could not connect: ' . $dbconn->connect_error);
-}
-//Use for custom error handle with $debug above.
-elseif($debug == "true"){
-	echo nl2br("\nDEBUG:\n");
-	echo nl2br("3 \n 2 \n 1...");
-	echo nl2br("\n Connected successfully\n");
+if ($conn->connect_error){
+    die('Could not connect:'.$db.conn->connect_error);
 }
 
-//You can use the command below to set the default database to another db.
-//mysqli_select_db($dbconn, "webiii");
+//elseif($debug == "true"){
+//    echo nl2br("\nDEBUG:\n");
+//    echo nl2br("3 \n 2 \n 1...");
+//    echo nl2br("\nConnected Successfully\n");
+//}
 
 ?>
