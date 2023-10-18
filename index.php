da08ae6763c6f64fa5a61ed1b33ee67c5ffe85a2
jacylin873 <98434882+jacylin873@users.noreply.github.com>
2023-10-18 13:37:16 -0400

    Add files via upload
    Added comments to PHP code.

--- index.php
+++ index.php
@@ -1,30 +1,35 @@
 <html>
+    <!-- The beginning steps (header) of declaring a HTML/PHP website (Jacy) -->
     <head>
+        <!-- The title to the website that shows up on the navigation bar (Jacy) -->
         <title>Edu Planner</title>
 
-        <!-- Links the CSS file to the PHP file -->
+        <!-- Links the CSS file to the PHP file (Jacy) -->
         <?php echo '<link rel="stylesheet" type="text/css" href="style.css"></head>'; ?>
     </head>
 
+    <!-- Body code for the structure of the website (Jacy) -->
     <body>
+        <!-- Div class code for Login title and Username Box (Jacy) -->
         <div class="login-box">
             <h2>Login</h2>
             <form method="post" action="">
-              <div class="user-box">
-                <input type="text" name="email">
-                <label>Username</label>
-              </div>
-              <div class="user-box">
-                <input type="password" name="password">
-                <label>Password</label>
-              </div>
-              <input type="submit" name="submit">
-            
+            <div class="user-box">
+              <input type="text" name="email">
+              <label>Username</label>
+            </div>
+            <!-- Div class clode the the Password Box (Jacy) -->
+            <div class="user-box">
+              <input type="password" name="password">
+              <label>Password</label>
+            </div>
+            <!-- Submission box for inputing Username and Password (Jacy) -->
+            <input type="submit" name="submit">
             </form>
             <div class = "echo-statement">
-            <?php include("loginArithmetic.php"); ?>
+              <?php include("loginArithmetic.php"); ?>
             </div>
-          </div>
+        </div>
     </body>
     </style>
 
