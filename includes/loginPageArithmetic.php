58fe5bddb68da31ddcae143c3d8a7743c0f56d70
angelfo7319 <144272766+angelfo7319@users.noreply.github.com>
2023-11-15 19:20:22 -0500

    added user class

--- includes/loginPageArithmetic.php
+++ includes/loginPageArithmetic.php
@@ -1,7 +1,6 @@
 <?php
-session_start(); 
-//Connects to the database.
-include("connect.php");
+session_start();
+
 //checks if $_POST varibles already exist or Cookie exists
 if (isset($_POST['user_email']) && isset($_POST['user_password'])) {
 //Sanitizes the user information submitted before sending into database
@@ -14,71 +13,26 @@ if (isset($_POST['user_email']) && isset($_POST['user_password'])) {
 //Sanitizes the submitted email
     $user_email = check($_POST['user_email']);
 //Sanitizes the submitted password
-    $password = check($_POST['user_password']);
+    $user_password = check($_POST['user_password']);
 //Generates error message if email field is empty
     if (empty($user_email)) {
         header("Location: ../login.php?error=Email is required");
         exit();
-    } 
+    }
 //Generates error message if password field is empty
-    else if(empty($password)){
+    else if(empty($user_password)){
         header("Location: ../login.php?error=Password is required");
         exit();
-    } 
-//Continue if the passowrd field and email field are empty    
-    else{
-//SQL command to select from database saved user which matches inputs
-        $sql = "SELECT * FROM user_profile WHERE user_email ='$user_email' AND user_password ='$password'";
-        $result = mysqli_query($conn, $sql);
-//Continue if result if found; return to login if not
-        if (mysqli_num_rows($result) === 1) {
-            $row = mysqli_fetch_assoc($result);
-//Double checks that the value in the database is equal to inputted values
-            if ($row['user_email'] === $user_email && $row['user_password'] === $password) {
-//Turns important fields into $_Session variables to be checked by following pages
-                $_SESSION['user_email'] = $row['user_email'];
-                $_SESSION['f_name'] = $row['f_name'];
-                $_SESSION['UPID'] = $row['UPID'];
-                $_SESSION['logged_user'] = $row;
-                $_SESSION['clearance'] = $row['clearance'];
-//Sets user data as a cookie
-                $cookie_name = "eduPlanner_logged_user";
-                $cookie_value = serialize($_SESSION['logged_user']);
-                //unserialize() used to convert back to array
-                setcookie($cookie_name, $cookie_value, time() + 3600, "/");
-//Sends user to admin page if clearance level is 0
-                if ($_SESSION['clearance'] == 0){
-                    header("Location: ../administration/adminHome.php");
-                    exit();
-                    }            
-//Sends user to faculty page if clearance level is 1    
-                else if ($_SESSION['clearance'] == 1){
-                    header("Location: ../faculty/facultyHome.php");
-                    exit();
-                    } 
-//Sends user to student page if clearance level is 2               
-                else if ($_SESSION['clearance'] == 2){
-                    header("Location: ../student/studentHome.php");
-                    exit();
-                }
-//Sends user back to login page if clearance level is nonexistent
-                else{
-                    header("Location: ../login.php");
-                    exit();
-                }
-            }
-//Sends user back to login page            
-            else{
-                header("Location: ../login.php?error=Incorect Email or password");
-                exit();
-            }
-        }
-//Sends user back to login page        
-        else{
-            header("Location: ../login.php?error=Incorect Email or password");
-            exit();
-        }
     }
+//Continue if the password field and email field are empty    
+    else{
+        //Call user class
+        include_once("../classes/userClass.php");
+        //Create new user object
+        $user = new User();
+        //Call login_User method to check if user exists in database
+        $user->login_User($user_email, $user_password);
+}
 }
 //Sends user back to login page
 else{
