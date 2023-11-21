31c7290c3a277c568fce0277d4a3907ba62d91cc
angelfo7319 <144272766+angelfo7319@users.noreply.github.com>
2023-11-20 19:10:48 -0500

    created schedule class

---
+++ classes/scheduleClass.php
@@ -0,0 +1,20 @@
+<?php
+
+//Global function to include courses class in another file.
+//include_once("classes/scheduleClass.php");
+
+Class Schedule{
+  private $SID;
+  private $UPID;
+  public $CLID_1;
+  public $CLID_2;
+  public $CLID_3;
+  public $CLID_4;
+  public $CLID_5;
+  public $CLID_6;
+  public $CLID_7;
+  public $CLID_8;  
+
+}
+
+?>
