ef29631917d1a1a7f216867a7e7b6f2ee5b9a554
Jacy Lin <98434882+jacylin873@users.noreply.github.com>
2023-12-10 22:01:27 -0500

    Updated footer
    Made it responsive.

--- footer.php
+++ footer.php
@@ -1,56 +1,49 @@
 <p>
 <!DOCTYPE html>
 <html>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <link rel="stylesheet" href="css/footer.css">
 
-<footer class="section footer-classic context-dark bg-image" style="background: #0d1d5e;">
-        <div class="container">
-          <div class="row row-30">
-            <div class="col-md-4 col-xl-5">
-                <p>SUNY New Paltz is a competitive, four-year university college that delivers an extraordinary number of undergraduate majors in business, liberal arts & sciences, fine and performing arts and education. Our programs in the STEM fields are diverse and cutting-edge.</p>
-                <!-- Rights-->
-                <p class="rights"><span>©  </span><span class="copyright-year">2023</span><span> </span><span>SUNY New Paltz</span><span> - </span><span>All Rights Reserved.</span></p>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <h5>Contacts</h5>
-              <dl class="contact-list">
-                <dt>Address</dt>
-                <dd>1 Hawk Dr, New Paltz, NY, 12561</dd>
-              </dl>
-              <dl class="contact-list">
-                <dt>Email</dt>
-                <dd><a href="mailto:#">admissions@newpaltz.edu</a></dd>
-              </dl>
-              <dl class="contact-list">
-                <dt>Phone Numbers</dt>
-                <dd><a href="tel:#">845-257-7869</a> <span>or</span> <a href="tel:#">877-MY-NP-411</a>
-                </dd>
-              </dl>
-            </div>
-            <div class="col-md-4 col-xl-3">
-              <h5>Links</h5>
-              <ul class="nav-list">
-                <li><a href="http://www.newpaltz.edu/advising/">Academic Advising</a></li>
-                <li><a href="https://www.newpaltz.edu/registrar/">Records and Registration</a></li>
-                <li><a href="https://www.newpaltz.edu/classes/abbreviations.html">Abbreviations explained</a></li>
-                <li><a href="http://www.newpaltz.edu/academics/descriptions.html">Course Descriptions</a></li>
-                <li><a href="http://www.newpaltz.edu/graduate/catalog.html">Graduate Catalog</a></li>
-                <li><a href="http://www.newpaltz.edu/ugc/">Undergraduate Catalog</a></li>
-              </ul>
-            </div>
-          </div>
+      <footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        <p class="footer-links">
+          <a href="http://www.newpaltz.edu/advising/" class="link-1">Academic Advising</a>
+          
+          <a href="https://www.newpaltz.edu/registrar/">Records and Registration</a>
+        
+          <a href="https://www.newpaltz.edu/classes/abbreviations.html">Abbreviations Explained</a>
+        
+          <a href="http://www.newpaltz.edu/academics/descriptions.html">Course Descriptions</a>
+          
+          <a href="http://www.newpaltz.edu/graduate/catalog.html">Graduate Catalog</a>
+          
+          <a href="http://www.newpaltz.edu/ugc/">Undergraduate Catalog</a>
+        </p>
+        <p class="footer-company-name">State University of New York at New Paltz © 2023</p>
+      </div>
+      <div class="footer-center">
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span>1 Hawk Dr</span> New Paltz, NY, 12561</p>
         </div>
-        <div class="row no-gutters social-container">
-          <div class="col"><a class="social-inner" href="http://www.facebook.com/newpaltz"><span class="icon mdi mdi-facebook"></span><span>Facebook</span></a></div>
-          <div class="col"><a class="social-inner" href="http://instagram.com/sunynewpaltz"><span class="icon mdi mdi-instagram"></span><span>Instagram</span></a></div>
-          <div class="col"><a class="social-inner" href="http://www.twitter.com/newpaltz"><span class="icon mdi mdi-twitter"></span><span>Twitter</span></a></div>
-          <div class="col"><a class="social-inner" href="https://www.youtube.com/user/sunynewpaltz"><span class="icon mdi mdi-youtube-play"></span><span>Youtube</span></a></div>
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>(845) 257-7869</p>
         </div>
-      </footer>
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="mailto:admissions@newpaltz.edu">admissions@newpaltz.edu</a></p>
+        </div>
+      </div>
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <span>About SUNY New Paltz</span>
+          SUNY New Paltz is a competitive, four-year university college that delivers an extraordinary number of undergraduate majors in business, liberal arts & sciences, fine and performing arts and education. Our programs in the STEM fields are diverse and cutting-edge.
+        </p>
+      </div>
+    </footer>
+
 </html>
 </p>
